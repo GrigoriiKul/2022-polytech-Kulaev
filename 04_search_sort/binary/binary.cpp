f9aa69a8cec8b60ab34557565a279812b6f0fb98
Григорий Кулаев <kulaev.grisha@yandex.ru>
2022-12-02 12:23:48 +0300

    add(04):create binary

---
+++ 04_search_sort/binary/binary.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int search(int arr[], int x, int y, int key)
+{
+	int mi;
+	bool flag = false;
+	while ((x <= y) && (flag != true)) {
+		mi = (x + y) / 2;
+
+		if (arr[mi] == key) flag = true; 
+		if (arr[mi] > key) y = mi - 1; 
+		else x = mi + 1;
+	}
+	return mi;
+}
+int main()
+{
+    int m[11] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}; 
+    const int arrsize = sizeof(m) / sizeof(m[0]); // находим длину массива
+    cout << "Item ID: " << search(m, 0, arrsize, 20) << endl; // выводим количество найденных элементов и их номера
+}
